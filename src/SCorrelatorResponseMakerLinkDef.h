b43c0bad7b56316e867a2c08f5d1bb4f22425bf9
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-08-01 04:50:21 -0400

    Make comments consistent

--- src/SCorrelatorResponseMakerLinkDef.h
+++ src/SCorrelatorResponseMakerLinkDef.h
@@ -1,13 +1,14 @@
-// ----------------------------------------------------------------------------
-// 'SCorrelatorResponseMakerLinkDef.h'
-// Derek Anderson
-// 04.30.2023
-//
-// A module to match truth to reconstructed
-// jets/particles to derive corrections for
-// an n-point energy correlation strength
-// function.
-// ----------------------------------------------------------------------------
+/// ---------------------------------------------------------------------------
+/*! \file   SCorrelatorResponseMakerLinkDef.h
+ *  \author Derek Anderson
+ *  \date   04.30.2023
+ *
+ * A module to match truth to reconstructed
+ * jets/particles to derive corrections for
+ * an n-point energy correlation strength
+ * function.
+ */
+/// ---------------------------------------------------------------------------
 
 #ifdef __CINT__
 
